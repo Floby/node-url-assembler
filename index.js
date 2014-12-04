513a2d807c9f9cb5ddf0ec1ee42de8f720158bca
Florent Jaby <fjaby@octo.com>
2014-12-04 11:23:45 +0100

    unbound param in querystring

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+var qs = require('qs');
+
 module.exports = UrlAssembler;
 
 function UrlAssembler (_template) {
   if(!(this instanceof UrlAssembler)) return new UrlAssembler(_template);
   if(!_template) throw new Error('template parameter is mandatory');
   var template = _template;
+  var query = {};
 
+  this.param = function (key, value) {
+    var symbol = ':' + key;
+    previous = template;
+    template = template.replace(symbol, value);
+    if(previous === template) {
+      query[key] = value
+    }
+    return this;
+  }
   this.prefix = function (prefix) {
     template = prefix + template;
     return this;
   };
   this.toString = function () {
-    return template;
+    var q = qs.stringify(query);
+    if(q) q = '?' + q;
+    return template + q
   };
 }
 
--- test/100-instance.js
+++ test/100-instance.js
@@ -18,6 +18,23 @@ describe('an instance', function () {
       it('adds a prefix to the result of toString()', function () {
         expect(myUrl.prefix('/coucou').toString()).to.equal('/coucou/hello');
       });
+    });
+
+    describe('.param()', function () {
+      it('adds the parameter as a query parameter', function () {
+        expect(myUrl.param('a', 'bc').toString()).to.equal('/hello?a=bc');
+      });
+    });
+  });
+
+  describe('given a template with a simple parameter', function () {
+    beforeEach(function () {
+      myUrl = UrlAssembler('/path/:myparam');
     })
+    describe('.param()', function () {
+      it('replaces the parameter in the template', function () {
+        expect(myUrl.param('myparam', 'hello').toString()).to.equal('/path/hello');
+      })
+    });
   });
 });
