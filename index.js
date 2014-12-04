ae1ed1618653636d926fa373c1a36b3fecdcf574
Florent Jaby <fjaby@octo.com>
2014-12-04 11:58:46 +0100

    refactor

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
+var extend = require('extend');
+var url = require('url');
 var qs = require('qs');
 
 module.exports = UrlAssembler;
 
-function UrlAssembler (_template) {
-  if(!(this instanceof UrlAssembler)) return new UrlAssembler(_template);
-  if(!_template) throw new Error('template parameter is mandatory');
-  var template = _template;
-  var query = {};
+function UrlAssembler (template) {
+  if(!(this instanceof UrlAssembler)) return new UrlAssembler(template);
+  if(!template) throw new Error('template parameter is mandatory');
 
-  this.param = function (key, value) {
-    var symbol = ':' + key;
-    previous = template;
-    template = template.replace(symbol, value);
-    if(previous === template) {
-      query[key] = value
-    }
+  extend(this, url.parse(template));
+
+  var query = {};
+  this.search = null;
+  this.query = function addQueryParam (key, value) {
+    query[key] = value;
+    var search = qs.stringify(query);
+    if(search) search = '?' + search;
+    this.search = search;
     return this;
   }
-  this.prefix = function (prefix) {
-    template = prefix + template;
-    return this;
-  };
-  this.toString = function () {
-    var q = qs.stringify(query);
-    if(q) q = '?' + q;
-    return template + q
-  };
 }
 
+var m = UrlAssembler.prototype;
+
+m.toString = function toString () {
+  return url.format(this);
+};
+
+m.prefix = function prefix (prefix) {
+  this.pathname = prefix + this.pathname;
+  return this;
+};
+
+m.param = function param (key, value) {
+  var previous = this.pathname;
+  var symbol = ':' + key;
+  this.pathname = this.pathname.replace(symbol, value);
+  if(this.pathname === previous) {
+    this.query(key, value);
+  }
+  return this;
+};
